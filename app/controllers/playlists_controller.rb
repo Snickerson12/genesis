448819fb90f629d867d44b147f7d2f3329ad5e2a
github username <github email address>
2019-10-02 09:54:10 -0500

    adjusted home navigation and removed excess buttons

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,4 +1,5 @@
 class PlaylistsController < ApplicationController
+    before_action :require_logged_in
     before_action :find_playlist, only: [:show, :edit, :update, :destroy, :share, :share_playlist]
 
     def index
